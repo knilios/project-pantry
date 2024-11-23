e6254b23a5a3624a3f90dd251e8b78b3319f2e54
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-11-23 16:58:18 +0700

    Remove dj_database_url

--- pantry/settings.py
+++ pantry/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config, Csv
-import dj_database_url
 from django.db.backends import postgresql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
